83efddbf42680996d7fc78c104dcaae3dfa186b6
DanyHerz42 <dhernandez30@ucol.mx>
2019-06-11 12:34:44 -0500

    eliminar

--- js/formularios.js
+++ js/formularios.js
@@ -1,17 +1,19 @@
 import Nota from "./nota.js"
-export default class Formulario{
-    constructor(formulario){
+export default class Formulario {
+    constructor(formulario) {
         this._type = formulario;
+        this._body = document.querySelector("#body");
     }
 
-    crearFormulario(){
-        if (this._type === "registro"){
+    crearFormulario(edicion) {
+        if (this._type === "registro") {
             this._crearRegistro();
+        } else {
+            this._crearEdicion(edicion);
         }
-        
     }
 
-    _crearRegistro(){
+    _crearEdicion(edicion) {
         let divBlack = document.createElement('div');
         divBlack.className = "divBlack";
         let container = document.createElement('div');
@@ -28,7 +30,8 @@ export default class Formulario{
         lbName.style.color = "white"
         lbName.textContent = "Name: "
         inpName.type = "text";
-        inpName.required =  true;
+        inpName.required = true;
+        inpName.value = edicion.name;
         inpName.placeholder = "Type here";
         inpName.className = "form-control";
         let validFeedback = document.createElement('div');
@@ -45,7 +48,8 @@ export default class Formulario{
         lbDate.style.color = "white"
         lbDate.textContent = "Date of term";
         inpDate.type = "date";
-        inpDate.required =  true;
+        inpDate.required = true;
+        inpDate.value = this._obtenerFechaString(edicion.date);
         inpDate.className = "form-control";
         let validFeedback2 = document.createElement('div');
         validFeedback2.textContent = "Correcto";
@@ -60,14 +64,154 @@ export default class Formulario{
         btnSave.style.marginRight = '10px';
         btnSave.type = 'button';
         btnSave.addEventListener('click', () => {
-            if(form.checkValidity() === true){
+            if (form.checkValidity() === true) {
+                let objEdit = {
+                    name: inpName.value,
+                    date: inpDate.value
+                }
+                document.querySelector('body').removeChild(divBlack);
+                let n = new Nota(objEdit);
+                n._editarNota(edicion);  
+            }
+            form.classList.add('was-validated');
+
+        });
+
+        let btnDel = document.createElement('input');
+        btnDel.className = 'btn btn-danger';
+        btnDel.value = 'C A N C E L'
+        btnDel.type = 'button';
+        btnDel.addEventListener('click', () => {
+            document.querySelector('body').removeChild(divBlack);
+        });
+
+        formGroup1.appendChild(lbName);
+        formGroup1.appendChild(inpName);
+        formGroup1.appendChild(validFeedback);
+        formGroup1.appendChild(invalidFeedback);
+        formGroup2.appendChild(lbDate);
+        formGroup2.appendChild(inpDate);
+        formGroup2.appendChild(validFeedback2);
+        formGroup2.appendChild(invalidFeedback2);
+        form.appendChild(formGroup1);
+        form.appendChild(formGroup2);
+        form.appendChild(btnSave);
+        form.appendChild(btnDel);
+        container.appendChild(form);
+        divBlack.appendChild(container)
+        document.querySelector('body').appendChild(divBlack);
+    }
+
+    _obtenerFechaString(date) {
+        let dateE = date;
+        dateE = dateE.split("/");
+        let fechaCons = dateE[2] + "-" + "0" + this._convertirMesesANum(dateE[1]) + "-" + dateE[0];
+        return fechaCons;
+    }
+
+    _convertirMesesANum(mes) {
+        let newMes = 0;
+        switch (mes) {
+            case "Ene":
+                newMes = 1;
+                break;
+            case "Feb":
+                newMes = 2;
+                break;
+            case "Mar":
+                newMes = 3;
+                break;
+            case "Abr":
+                newMes = 4;
+                break;
+            case "May":
+                newMes = 5;
+                break;
+            case "Jun":
+                newMes = 6;
+                break;
+            case "Jul":
+                newMes = 7;
+                break;
+            case "Ago":
+                newMes = 8;
+                break;
+            case "Sep":
+                newMes = 9;
+                break;
+            case "Oct":
+                newMes = 10;
+                break;
+            case "Nov":
+                newMes = 11;
+                break;
+            case "Dic":
+                newMes = 12;
+                break;
+        }
+
+        return newMes;
+    }
+
+    _crearRegistro() {
+        let divBlack = document.createElement('div');
+        divBlack.className = "divBlack";
+        let container = document.createElement('div');
+        container.className = "formRegistro";
+
+        let form = document.createElement('form');
+        form.className = "needs-validation p-4";
+        form.noValidate = true;
+
+        let formGroup1 = document.createElement('div');
+        formGroup1.className = "form-group";
+        let inpName = document.createElement('input');
+        let lbName = document.createElement('label');
+        lbName.style.color = "white"
+        lbName.textContent = "Name: "
+        inpName.type = "text";
+        inpName.required = true;
+        inpName.placeholder = "Type here";
+        inpName.className = "form-control";
+        let validFeedback = document.createElement('div');
+        validFeedback.textContent = "Correcto";
+        validFeedback.className = 'valid-feedback';
+        let invalidFeedback = document.createElement('div');
+        invalidFeedback.textContent = 'Please complete the field';
+        invalidFeedback.className = 'invalid-feedback';
+
+        let formGroup2 = document.createElement('div');
+        formGroup2.className = "form-group";
+        let inpDate = document.createElement('input');
+        let lbDate = document.createElement('label');
+        lbDate.style.color = "white"
+        lbDate.textContent = "Date of term";
+        inpDate.type = "date";
+        inpDate.required = true;
+        inpDate.className = "form-control";
+        let validFeedback2 = document.createElement('div');
+        validFeedback2.textContent = "Correcto";
+        validFeedback2.className = 'valid-feedback';
+        let invalidFeedback2 = document.createElement('div');
+        invalidFeedback2.textContent = 'Please complete the field';
+        invalidFeedback2.className = 'invalid-feedback';
+
+        let btnSave = document.createElement('input');
+        btnSave.className = 'btn btn-success';
+        btnSave.value = 'S A V E';
+        btnSave.style.marginRight = '10px';
+        btnSave.type = 'button';
+        btnSave.addEventListener('click', () => {
+            if (form.checkValidity() === true) {
                 let objNota = {
                     name: inpName.value,
                     date: inpDate.value
                 }
                 let n = new Nota(objNota);
+                document.querySelector('body').removeChild(divBlack);
                 n.saveOnLocalStorage();
             }
+
             form.classList.add('was-validated')
         });
 
--- js/nota.js
+++ js/nota.js
@@ -1,3 +1,4 @@
+import Formulario from "./formularios.js"
 import Vista from "./vista.js";
 export default class Nota {
     constructor(nota) {
@@ -23,10 +24,47 @@ export default class Nota {
         fecha.textContent = e.date;
         fecha.className = "fechaNota";
 
+        let btnDelete = document.createElement('i');
+        btnDelete.className = 'fas fa-trash-alt delete';
+        btnDelete.addEventListener('click', () => {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.value) {
+                    this._eliminarNota(e.name);
+                    this._entorno.removeChild(div);
+                    Swal.fire(
+                        'Deleted!',
+                        'This person has been deleted.',
+                        'success'
+                    )
+                }
+            })
+        })
+
+        let btnEdit = document.createElement('i');
+        btnEdit.className = 'far fa-edit edit';
+        btnEdit.addEventListener('click', () => {
+            let objEdit = {
+                name: e.name,
+                date: e.date
+            }
+            let formulario = new Formulario("edicion");
+            formulario.crearFormulario(objEdit);
+        })
+
         div.appendChild(name);
         div.appendChild(tachuela);
         div.appendChild(hr);
         div.appendChild(fecha);
+        div.appendChild(btnDelete);
+        div.appendChild(btnEdit);
         this._entorno.appendChild(div);
     }
 
@@ -52,7 +90,7 @@ export default class Nota {
             aNotas.forEach((e, index) => {
                 console.log("hola")
                 if (e.name === objNota.name) {
-                    
+
                     Swal.fire({
                         title: 'Error!',
                         text: 'This note is already added!',
@@ -78,7 +116,7 @@ export default class Nota {
         }
         localStorage.setItem("notas", JSON.stringify(aNotas));
         vista._generarVista(aNotas);
-        
+
     }
 
     _obtenerFechaString(fecha) {
@@ -105,4 +143,45 @@ export default class Nota {
         let age = Math.trunc(differenceMs / oneDay);
         return age;
     }
+
+    _eliminarNota(name) {
+        let aCont = JSON.parse(localStorage.getItem("notas"));
+        aCont.forEach((e, index) => {
+            if (name === e.name) {
+                
+                aCont.splice(index, 1);
+            }
+        });
+        localStorage.setItem("notas", JSON.stringify(aCont));
+    }
+
+    _editarNota(edicion) {
+        let aNotas = [];
+        if (!(localStorage.getItem("notas") === null)) {
+            aNotas = JSON.parse(localStorage.getItem("notas"));
+        }
+        let objNota = {
+            name: this._name,
+            date: this._obtenerFechaString(this._date),
+            days: this._calcularDias(this._obtenerObjetoFecha(this._date))
+        }
+        console
+        aNotas.forEach((e, index) => {
+            if (e.name === edicion.name) {
+                console.log(e);
+                aNotas.splice(index, 1, objNota);
+            }
+        })
+        localStorage.setItem('notas', JSON.stringify(aNotas));
+        Swal.fire({
+            title: 'Ready!',
+            text: 'workshop successfully edited!',
+            type: 'success',
+            confirmButtonText: 'OK'
+        })
+        let vista = new Vista(document.querySelector('#entorno'));
+        vista._generarVista(aNotas);
+    };
+
 }
+
